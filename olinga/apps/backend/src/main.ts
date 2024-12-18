4d3ddce8fd7a9492412ac19230e6a09b88696465
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-12-18 21:28:26 +0100

    remove tests

--- olinga/apps/backend/src/main.ts
+++ olinga/apps/backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());

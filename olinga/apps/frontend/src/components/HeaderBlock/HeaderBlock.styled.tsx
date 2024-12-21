3222102a3232c6561b0bb2fd45306ea6455be4ae
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-12-21 23:03:36 +0100

    Header

--- olinga/apps/frontend/src/components/HeaderBlock/HeaderBlock.styled.tsx
+++ olinga/apps/frontend/src/components/HeaderBlock/HeaderBlock.styled.tsx
@@ -1,53 +1,231 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-export const HeaderContainer = styled.header`
-  width: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45));
-  background-repeat: no-repeat;
-  background-size: cover;
-  padding-top:10px;
-  padding-bottom: 10px;
-`;
-
-export const HeaderLine = styled.div`
-  width: 100%;
-  padding: 0 2.5%;
+export const HeaderWrapper = styled.header`
+  font-family: 'Montserrat', sans-serif;
+  padding: 0 2%;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  background-color: #1e1e1e;
   position: fixed;
   top: 0;
   left: 0;
-  background-color: #1e1e1e;
-  z-index: 999;
+  z-index: 998;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
 `;
 
 export const LogoWrapper = styled.div`
   width: 14%;
+  display: flex;
+  align-items: center;
   img {
-    object-fit: contain;
     width: 100%;
+    filter: brightness(1.5);
     height: auto;
+    transition: filter 0.3s ease, transform 0.3s ease;
+    &:hover {
+      filter: brightness(2);
+      transform: scale(1.05);
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 20%;
+  }
+
+  @media only screen and (max-width: 426px) {
+    width: 40%;
   }
 `;
 
 export const HeaderBox = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  gap: 20px;
+
+  @media only screen and (max-width: 425px) {
+    gap: 5px;
+  }
 `;
 
-export const Button = styled.button`
-  background-color: #fff;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  font-size: 16px;
+export const ContactUsBlock = styled.div`
+  position: relative;
+  transition: color 0.3s ease;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    transition: color 0.3s ease;
+
+    &:hover {
+      color: #c6caa6;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const ContactItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
   cursor: pointer;
-  transition: background-color 0.3s ease;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 500;
+  transition: color 0.3s ease;
 
   &:hover {
-    background-color: #e4e4e4;
+    color: #c6caa6;
+  }
+`;
+
+export const ContactNumber = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    &:hover {
+      color: #c6caa6;
+    }
+  }
+`;
+
+export const DropdownContacts = styled.div`
+  position: absolute;
+  top: 240%;
+  left: 10%;
+  background: #292929;
+  padding: 10px;
+  border-radius: 5px;
+  width: 220px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  animation: fadeIn 0.3s ease forwards;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+export const DropdownItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  padding: 5px 0;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  &:hover {
+    color: #c6caa6;
+  }
+`;
+
+export const MenuBlock = styled.div`
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #c6caa6;
+  }
+
+  @media only screen and (max-width: 425px) {
+    margin-left: 0px;
+  }
+`;
+
+export const LanguegeBlock = styled.div`
+  position: relative;
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #c6caa6;
+  }
+`;
+
+export const Languege = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    &:hover {
+      color: #c6caa6;
+    }
+  }
+`;
+
+export const DropdownLanguages = styled.div`
+  position: absolute;
+  top: 250%;
+  left: 0;
+  background: #292929;
+  padding: 10px;
+  border-radius: 5px;
+  width: 45px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  animation: fadeIn 0.3s ease forwards;
+  z-index: 999;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+export const LanguageOption = styled.div<{ isSelected: boolean }>`
+  display: flex;
+  color: ${({ isSelected }) => (isSelected ? '#c6caa6' : '#ffffff')};
+  align-items: center;
+  padding: 8px 12px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  background-color: ${({ isSelected }) =>
+    isSelected ? '#444' : 'transparent'};
+  &:hover {
+    background-color: #444;
+    color: #c6caa6;
   }
 `;
--- olinga/apps/frontend/src/components/HeaderBlock/HeaderBlock.tsx
+++ olinga/apps/frontend/src/components/HeaderBlock/HeaderBlock.tsx
@@ -1,29 +1,134 @@
 import React, { useState } from "react";
-import { HeaderContainer, HeaderLine, LogoWrapper, HeaderBox, Button } from "./HeaderBlock.styled";
-import Navbar from "../NavBar";
+import { CgMenuRightAlt } from "react-icons/cg";
+import { GoTriangleDown } from "react-icons/go";
+import { FaPhoneAlt } from "react-icons/fa";
+import {
+  HeaderWrapper,
+  LogoWrapper,
+  HeaderBox,
+  ContactUsBlock,
+  ContactItem,
+  ContactNumber,
+  MenuBlock,
+  DropdownContacts,
+  DropdownItem,
+  LanguegeBlock,
+  Languege,
+  DropdownLanguages,
+  LanguageOption,
+} from "./HeaderBlock.styled";
+import { NavLink } from "react-router-dom";
+import NavBar from "../NavBar";
 
 const HeaderBlock: React.FC = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isContactsOpen, setContactsOpen] = useState(false);
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
+  const [isLanguagesOpen, setLanguagesOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("UA");
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+  const toggleNavbar = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const toggleContacts = () => {
+    setContactsOpen((prev) => !prev);
+  };
+
+  const toggleLanguages = (event: React.MouseEvent) => {
+    if (!isLanguagesOpen) {
+      setLanguagesOpen(true);
+    } else {
+      setLanguagesOpen(false);
+    }
+  };
+
+  const changeLanguage = (language: string) => {
+    setSelectedLanguage(language);
+    setLanguagesOpen(false);
   };
 
   return (
     <>
-      <HeaderContainer>
-        <HeaderLine>
-          <LogoWrapper>
-            <a href="/">
-              <img src="https://olinga.pl/public/img/logo1.webp" alt="Logo" />
-            </a>
-          </LogoWrapper>
-          <HeaderBox>
-            <Button onClick={toggleSidebar}>Menu</Button>
-          </HeaderBox>
-        </HeaderLine>
-      </HeaderContainer>
-      <Navbar isOpen={isSidebarOpen} onClose={toggleSidebar} />
+      <HeaderWrapper>
+        <LogoWrapper>
+          <NavLink to="/">
+            <img src="/public/logo1.webp" alt="Logo" />
+          </NavLink>
+        </LogoWrapper>
+
+        <HeaderBox>
+          <ContactUsBlock>
+            <ContactItem onClick={toggleContacts}>
+              <FaPhoneAlt size={20} />
+              <ContactNumber>
+                Адміністратор: <a href="tel:+48515732832">+48 515 732 832</a>
+              </ContactNumber>
+              <GoTriangleDown
+                size={16}
+                style={{
+                  transform: isContactsOpen ? "rotate(180deg)" : "rotate(0deg)",
+                  transition: "transform 0.3s ease",
+                }}
+              />
+            </ContactItem>
+            {isContactsOpen && (
+              <DropdownContacts>
+                <DropdownItem>
+                  <div>
+                    <FaPhoneAlt size={14} /> Олексій:
+                  </div>{" "}
+                  <a href="tel:+48515432123">+48 515 432 123</a>
+                </DropdownItem>
+                <DropdownItem>
+                  <div>
+                    <FaPhoneAlt size={14} /> Інга:
+                  </div>{" "}
+                  <a href="tel:+48765432123">+48 765 432 123</a>
+                </DropdownItem>
+              </DropdownContacts>
+            )}
+          </ContactUsBlock>
+
+          <LanguegeBlock onClick={toggleLanguages}>
+            <Languege>{selectedLanguage}</Languege>
+            <GoTriangleDown
+              size={16}
+              style={{
+                transform: isLanguagesOpen ? "rotate(180deg)" : "rotate(0deg)",
+                transition: "transform 0.3s ease",
+              }}
+            />
+            {isLanguagesOpen && (
+              <DropdownLanguages>
+                <LanguageOption
+                  isSelected={selectedLanguage === "UA"}
+                  onClick={() => changeLanguage("UA")}
+                >
+                  UA
+                </LanguageOption>
+                <LanguageOption
+                  isSelected={selectedLanguage === "PL"}
+                  onClick={() => changeLanguage("PL")}
+                >
+                  PL
+                </LanguageOption>
+                <LanguageOption
+                  isSelected={selectedLanguage === "RU"}
+                  onClick={() => changeLanguage("RU")}
+                >
+                  RU
+                </LanguageOption>
+              </DropdownLanguages>
+            )}
+          </LanguegeBlock>
+
+          <MenuBlock onClick={toggleNavbar}>
+            <CgMenuRightAlt size={30} />
+            <span>Меню</span>
+          </MenuBlock>
+        </HeaderBox>
+      </HeaderWrapper>
+      <NavBar isOpen={isNavbarOpen} onClose={() => setNavbarOpen(false)} />
     </>
   );
 };

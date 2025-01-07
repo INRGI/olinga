30b80e1311cc00c80cb42c00bb65fc8674d871cd
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-01-07 22:18:15 +0100

    services block

--- olinga/apps/frontend/src/components/ServicesBlock/ServicesBlock.styled.tsx
+++ olinga/apps/frontend/src/components/ServicesBlock/ServicesBlock.styled.tsx
@@ -1,39 +1,64 @@
 import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
   gap: 20px;
   padding: 20px;
   justify-content: center;
-  align-content: center;
-  align-items: center;
+  align-items: flex-start;
+  height: 948px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    height: auto;
+  }
 `;
 
 export const LeftContainer = styled.div`
   flex: 2;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  gap: 10px;
-  padding: 15px;
+  gap: 20px;
+  padding: 20px;
   max-width: 370px;
   background-color: #1e1e1e;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
+  justify-content: space-between;
+  align-self: stretch;
+
+  @media (max-width: 425px) {
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    form:nth-last-child(1) {
+      display: none;
+    }
+  }
 `;
 
 export const RightContainer = styled.div`
-  flex: 5;
+  flex: 3;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+
+  @media (min-width: 426px) and (max-width: 768px) {
+    flex: 1;
+    grid-template-columns: repeat(1, 1fr);
+
+    div:nth-child(n + 3) {
+      display: none;
+    }
+  }
+
+  @media (max-width: 425px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const ServicesBlockHeader = styled.div`
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  align-content: center;
   width: 100%;
 
   h2 {
@@ -41,7 +66,6 @@ export const ServicesBlockHeader = styled.div`
     line-height: 1.2;
     color: white;
     margin: 0;
-    padding: 0;
     font-weight: 500;
     font-family: 'Arial Black', sans-serif;
     text-transform: uppercase;
@@ -49,21 +73,17 @@ export const ServicesBlockHeader = styled.div`
   }
 
   p {
-    text-transform: uppercase;
     font-size: 18px;
     color: white;
-    margin: 0;
+    text-transform: uppercase;
     padding: 0;
-    padding-left: 2px;
+    margin: 0;
   }
 
-  @media (max-width: 1024px) {
-    h2 {
-      font-size: 20px;
-    }
-    p {
-      font-size: 15px;
-    }
+  @media (max-width: 425px) {
+    p{
+      font-size: 14px;
+    };
   }
 `;
 
@@ -71,7 +91,6 @@ export const LeftText = styled.p`
   font-size: 16px;
   color: white;
   margin: 0;
-  padding: 0;
 
   @media (max-width: 1024px) {
     font-size: 12px;
@@ -79,13 +98,89 @@ export const LeftText = styled.p`
 `;
 
 export const PromotionText = styled.p`
-  border-radius: 4px;
   font-size: 14px;
   font-weight: bold;
   color: black;
   margin: 0;
-  padding: 7px;
+  padding: 10px;
   background-color: #c6caa6;
   text-transform: uppercase;
   border-bottom: 4px solid #949874;
+
+  @media (max-width: 425px) {
+    font-size: 12px;
+  }
+`;
+
+export const CategoryCard = styled.div`
+  background-color: #2b2b2b;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
+  padding: 0;
+  text-align: left;
+  color: white;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding-bottom: 10px;
+
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+  }
+
+  h3 {
+    font-size: 20px;
+    margin: 0;
+    white-space: nowrap;
+    padding: 0 10px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  p {
+    font-size: 16px;
+    color: #c6c6c6;
+    margin: 0;
+    padding: 0 10px;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const Button = styled(NavLink)`
+  padding: 9px;
+  text-decoration: none;
+  background-color: #c6caa6;
+  color: black;
+  border: none;
+  border-radius: 6px;
+  font-size: 18px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  text-align: center;
+  max-width: 200px;
+  margin-left: 10px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  gap: 10px;
+  align-items: center;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+
+  @media (max-width: 425px) {
+    width: 100%;
+    max-width: 90%;
+  }
 `;
--- olinga/apps/frontend/src/components/ServicesBlock/ServicesBlock.tsx
+++ olinga/apps/frontend/src/components/ServicesBlock/ServicesBlock.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import { Container, LeftContainer, LeftText, PromotionText, RightContainer, ServicesBlockHeader } from './ServicesBlock.styled';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  CategoryCard,
+  Container,
+  LeftContainer,
+  LeftText,
+  PromotionText,
+  RightContainer,
+  ServicesBlockHeader,
+} from './ServicesBlock.styled';
 import { useTranslation } from 'react-i18next';
 import ConsultationForm from '../ConsultationForm';
+import { Category, getCategories } from '../../pages/AdminDashboard/CategoryService';
+import { TfiMore } from "react-icons/tfi";
 
 const ServicesBlock: React.FC = () => {
-    const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const data = await getCategories();
+      setCategories(data.slice(0, 4));
+    };
+
+    fetchCategories();
+  }, []);
+
+  if (categories.length === 0) return null; 
+
   return (
     <Container>
       <LeftContainer>
         <ServicesBlockHeader>
-            <h2>{t('ServicesBlock.head')}</h2>
-            <p>{t('ServicesBlock.subhead')}</p>
+          <h2>{t('ServicesBlock.head')}</h2>
+          <p>{t('ServicesBlock.subhead')}</p>
         </ServicesBlockHeader>
         <LeftText>{t('ServicesBlock.description')}</LeftText>
-
         <PromotionText>{t('ServicesBlock.promotion')}</PromotionText>
-
         <ConsultationForm />
       </LeftContainer>
 
-      <RightContainer></RightContainer>
+      <RightContainer>
+        {categories.map((category) => (
+          <CategoryCard key={category._id}>
+            {category.imageUrl && (
+              <img src={category.imageUrl} alt={category.title[i18n.language]} />
+            )}
+            <h3>{category.title[i18n.language]}</h3>
+            <p>{category.details[i18n.language]}</p>
+            <Button to={`/services/${category._id}`}>{t('courses.button')} <TfiMore /></Button>
+          </CategoryCard>
+        ))}
+      </RightContainer>
     </Container>
   );
 };

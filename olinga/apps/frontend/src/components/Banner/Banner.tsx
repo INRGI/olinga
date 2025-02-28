173fd568be9529c050397da71029e6280a5cc4d8
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-02-28 21:07:50 +0100

    links

--- olinga/apps/frontend/src/components/Banner/Banner.tsx
+++ olinga/apps/frontend/src/components/Banner/Banner.tsx
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
-import { Button, Container, ItemText, RightContainer, SocialContainer, SocialItem, TitleContainer } from './Banner.styled';
+import {
+  Button,
+  Container,
+  ItemText,
+  RightContainer,
+  SocialContainer,
+  SocialItem,
+  TitleContainer,
+} from './Banner.styled';
 import { useTranslation } from 'react-i18next';
 import Modal from '../Modal/Modal';
 import ConsultationForm from '../ConsultationForm';
-import { GoDotFill } from "react-icons/go";
+import { GoDotFill } from 'react-icons/go';
 import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
 
 const Banner: React.FC = () => {
-    const { t } = useTranslation();
-    const [isModalOpen, setModalOpen] = useState(false);
+  const { t, i18n } = useTranslation();
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const socialLinksFacebook: { [key: string]: string } = {
+    pl: 'https://www.instagram.com/olinga_massage/',
+    uk: 'https://www.instagram.com/olinga_massage/',
+    ru: 'https://www.instagram.com/olinga_massage/',
+  };
+
+  const socialLinksInstagram: { [key: string]: string } = {
+    pl: 'https://www.facebook.com/Massage.Katowice',
+    uk: 'https://www.facebook.com/masazystaoleksii',
+    ru: 'https://www.facebook.com/masazystaoleksii',
+  };
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
@@ -16,25 +36,49 @@ const Banner: React.FC = () => {
     <Container>
       <RightContainer>
         <TitleContainer>
-            <h2>{t('banner.head')}</h2>
-            <h3>{t('banner.subhead')}</h3>
+          <h2>{t('banner.head')}</h2>
+          <h3>{t('banner.subhead')}</h3>
         </TitleContainer>
 
-        <ItemText><GoDotFill />{t('banner.text1')}</ItemText>
-        <ItemText><GoDotFill />{t('banner.text2')}</ItemText>
-        <ItemText><GoDotFill />{t('banner.text3')}</ItemText>
-        <ItemText><GoDotFill />{t('banner.text4')}</ItemText>
+        <ItemText>
+          <GoDotFill />
+          {t('banner.text1')}
+        </ItemText>
+        <ItemText>
+          <GoDotFill />
+          {t('banner.text2')}
+        </ItemText>
+        <ItemText>
+          <GoDotFill />
+          {t('banner.text3')}
+        </ItemText>
+        <ItemText>
+          <GoDotFill />
+          {t('banner.text4')}
+        </ItemText>
 
         <Button onClick={openModal}>{t('banner.button')}</Button>
 
         <SocialContainer>
-          <SocialItem href="https://www.facebook.com/masazystaoleksii" target="_blank" rel="noopener noreferrer">
+          <SocialItem
+            href={socialLinksFacebook[i18n.language]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaFacebookSquare />
           </SocialItem>
-          <SocialItem href="https://www.instagram.com/olinga_massage?igsh=MXVlZmd5NmNvcHlmdQ==" target="_blank" rel="noopener noreferrer">
+          <SocialItem
+            href={socialLinksInstagram[i18n.language]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaInstagram />
           </SocialItem>
-          <SocialItem href="https://studiomasazuzdrowiaiurodyolinga.booksy.com/" target="_blank" rel="noopener noreferrer">
+          <SocialItem
+            href="https://studiomasazuzdrowiaiurodyolinga.booksy.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src="/booksy_review.png"
               alt="Booksy"
